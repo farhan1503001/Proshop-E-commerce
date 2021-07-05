00676f9927706aed6961f212452f445e7e5e371a
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-07-06 01:00:30 +0600

    after using django rest framework

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,10 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from .products import products
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+@api_view(['GET','POST'])
 def getRoutes(request):
     routes=['apps',
     '/apps/products/',
@@ -11,15 +14,18 @@ def getRoutes(request):
     '/apps/products/top',
     '/api/products/<id>'
     ]
-    return JsonResponse(routes,safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
 def get_products(request):
-    return JsonResponse(products,safe=False)
+    return Response(products)
+@api_view(['GET'])
 def get_product(request,pk):
     product=None
     for item in products:
         if item['_id']==pk:
             product=item
             break
-    return JsonResponse(product,safe=False)
+    return Response(product)
 
 
